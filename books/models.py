8e94dbce18d990c4248f806abe707b2dfb51351e
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-05-31 18:32:56 +0200

    implement models

--- books/models.py
+++ books/models.py
@@ -1,12 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
+
+import user
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(
-        max_length=10,
+        max_length=4,
         choices=[("Hard", "Soft")]
     )
     inventory = models.PositiveIntegerField()
@@ -16,18 +19,30 @@ class Book(models.Model):
         validators=[MinValueValidator(0.01)]
     )
 
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.inventory} available)"
+
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.book} ({self.expected_return_date})"
 
 
 class Payment(models.Model):
-    status = models.CharField()
-    type = models.CharField()
+    status = models.CharField(
+        max_length=7,
+        choices=[("pending", "paid")]
+    )
+    type = models.CharField(
+        max_length=7,
+        choices=[("payment", "fine")]
+    )
     borrowing_id = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
     session_url = models.URLField()
     session_id = models.IntegerField(unique=True)
@@ -36,3 +51,6 @@ class Payment(models.Model):
         decimal_places=2,
         validators=[MinValueValidator(0.01)]
     )
+
+    def __str__(self):
+        return f"{self.money_to_pay} ({self.status})"
