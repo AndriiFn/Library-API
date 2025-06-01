b35b7835607b5473a17e0fda8732daee94d26b42
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-06-01 17:58:25 +0200

    improve mdoels

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+
+from library_service import settings
 
 
 class Borrowing(models.Model):
@@ -7,7 +8,7 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user} borrowed {self.book} ({self.expected_return_date})"
