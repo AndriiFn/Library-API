37ffc9d999f2b1909d5683e704485899d473e5a9
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-06-01 17:58:54 +0200

    implement BorrowingCreateSerializer

--- borrowings/serializer.py
+++ borrowings/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from books.models import Book
 from books.serializers import BookSerializer
 from borrowings.models import Borrowing
 
@@ -16,3 +17,35 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
+
+    class Meta:
+        model = Borrowing
+        fields = ("book", "expected_return_date")
+
+    def validate(self, data):
+        book = data.get("book")
+        if not book:
+            raise serializers.ValidationError("Please select a book")
+
+        if book.inventory <= 0:
+            raise serializers.ValidationError("This book isn't available.")
+        return data
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        if not request:
+            raise serializers.ValidationError("You need to specify a request")
+
+        user = request.user
+        book = validated_data.get("book")
+
+        borrowing = Borrowing.objects.create(user=user, **validated_data)
+
+        book.inventory -= 1
+        book.save()
+
+        return borrowing
