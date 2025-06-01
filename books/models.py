6c4f70c7bde20095e4532c8db41790343783fb69
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-06-01 12:29:31 +0200

    remove Borrwing from models.py

--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
+
+from borrowing.models import Borrowing
 
 
 class Book(models.Model):
@@ -19,17 +20,6 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.inventory} available)"
-
-
-class Borrowing(models.Model):
-    borrow_date = models.DateField(auto_now=True)
-    expected_return_date = models.DateField()
-    actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.book} ({self.expected_return_date})"
 
 
 class Payment(models.Model):
