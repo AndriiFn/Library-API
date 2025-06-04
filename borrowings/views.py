7be2172c422fc3da37642fa590ca6bf1c6f269ca
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-06-04 14:25:20 +0200

    make sure admin can see borrowings of all users

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
 from borrowings.serializer import BorrowingSerializer, BorrowingCreateSerializer
@@ -6,6 +7,7 @@ from borrowings.serializer import BorrowingSerializer, BorrowingCreateSerializer
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("user", "book").all()
+    permission_classes = (IsAuthenticated, )
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -13,4 +15,7 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         return BorrowingSerializer
 
     def get_queryset(self):
+        if self.request.user.is_staff:
+            return Borrowing.objects.all()
+
         return Borrowing.objects.filter(user=self.request.user)
