8b3f9c99892b31bedcbcdf50bc3dfa06ff4b8d5d
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-06-07 14:08:22 +0200

    add Swagger

--- library_practice/urls.py
+++ library_practice/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +26,7 @@ urlpatterns = [
     path("api/user/", include("user.urls", namespace="user")),
     path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
     path("api/payment/", include("payment.urls", namespace="payment")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ] + debug_toolbar_urls()
